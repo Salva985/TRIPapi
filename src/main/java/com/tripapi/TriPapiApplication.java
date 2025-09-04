ca9a55c161f2dc441afed33f0229bdc834f0190e
salvatore marchese <smarchese985@yahoo.co.uk>
2025-09-04 11:45:56 +0200

    Initial Spring Boot setup

---
+++ src/main/java/com/tripapi/TriPapiApplication.java
@@ -0,0 +1,13 @@
+package com.tripapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriPapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriPapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripapi/TriPapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriPapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
